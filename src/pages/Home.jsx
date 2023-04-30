349dd5bf884be57a532b86c4ecc5684184b9d37c
Yura Popiuk <yurapopiuk8588585@gmail.com>
2023-04-30 16:21:06 +0300

    gg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ export const Home = () => {
               <Post
                 id={obj._id}
                 title={obj.title}
-                imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${data.imageUrl}` : ''}
+                imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
